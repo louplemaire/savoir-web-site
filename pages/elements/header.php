4d1902ea28688aa9b45b88a3ee194e2ce5bcb8e9
louplemaire <44206840+louplemaire@users.noreply.github.com>
2019-12-12 14:44:25 +0100

    :heavy_plus_sign: Pages > Create header

---
+++ pages/elements/header.php
@@ -0,0 +1,9 @@
+<header class="header">
+    <img src="../../assets/images/savoir-logo.png" alt="Savoir logo" class="header__logo">
+    <ul class="header__menu">
+        <li><a href="#">Explorer</a></li>
+        <li><a href="#">Créer un compte</a></li>
+        <li><a href="#">Intégrer SAVOIR</a></li>
+        <li><a href="#">À propos</a></li>
+    </ul>
+</header>
