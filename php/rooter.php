23c8d4df1673e5d09e9aedfb7b14549369f35887
louplemaire <44206840+louplemaire@users.noreply.github.com>
2019-12-12 15:22:28 +0100

    :wrench: Php > Fix rooter

--- php/rooter.php
+++ php/rooter.php
@@ -1,7 +1,7 @@
 <?php
-$url = (key_exists('REDIRECT_URL',$_SERVER)) ? $_SERVER['REDIRECT_URL'] : "";
+$url = (key_exists('REQUEST_URI',$_SERVER)) ? $_SERVER['REQUEST_URI'] : "";
 if ($url == "" || $url == "/") {
     include_once 'pages/accueil.php';
 } else {
-    include_once 'pages'.$_SERVER['REDIRECT_URL'].'.php';
+    include_once 'pages'.$_SERVER['REQUEST_URI'].'.php';
 }
