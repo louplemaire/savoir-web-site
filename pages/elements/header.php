6526aa0dc9851f89c77c0230d1da5901674793d7
louplemaire <44206840+louplemaire@users.noreply.github.com>
2019-12-12 15:24:19 +0100

    :wrench: Pages > Header > Fix a link from header to explorer page

--- pages/elements/header.php
+++ pages/elements/header.php
@@ -1,7 +1,7 @@
 <header class="header">
     <img src="../../assets/images/savoir-logo.png" alt="Savoir logo" class="header__logo">
     <ul class="header__menu">
-        <li><a href="#">Explorer</a></li>
+        <li><a href="/explorer">Explorer</a></li>
         <li><a href="#">Créer un compte</a></li>
         <li><a href="#">Intégrer SAVOIR</a></li>
         <li><a href="#">À propos</a></li>
