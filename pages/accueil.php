4b88bdfe4fb38fd98fe751190f8127f38c688669
Romain Penchenat <romain.penchenat@gmail.com>
2019-12-13 11:05:56 +0100

    push nico

---
+++ pages/accueil.php
@@ -0,0 +1,10 @@
+<main class="landingMain">
+    <div class="landingMain__middleContainer">
+        <h2 class="landingMain__middleContainer__landingTitle">
+            Apprenez,<br>
+            Partagez.
+        </h2>
+        <p class="landingMain__middleContainer__landingMainText">Devenez acteur de la nouvelle économie de la connaissance</p>
+        <a href="#" class="primaryButton"></a>
+    </div>
+</main>
