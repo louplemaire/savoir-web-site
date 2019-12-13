16036c7c54588b66d14af88d3aa639a755f4d844
louplemaire <44206840+louplemaire@users.noreply.github.com>
2019-12-13 09:45:36 +0100

    :wrench: Pages > Add explorer

---
+++ pages/explorer.php
@@ -0,0 +1,20 @@
+<main class="exploreMain">
+    <div class="exploreMain__exploreContainer">
+        <div class="exploreMain__exploreContainer__explorer">
+            <input class="exploreMain__exploreContainer__explorer__researcher" type="text" placeholder="Recherchez un utilisateur…">
+            <div class="exploreMain__exploreContainer__explorer__subResearcher"></div>
+        </div>
+    </div>
+    <div class="exploreMain__exploreContainer__tokenTraffic">
+            <div class="exploreMain__exploreContainer__tokenTraffic__info">
+                <div class="exploreMain__exploreContainer__tokenTraffic__info__number">167 000</div>
+                <div>tokens SAVOIR en circulation</div>
+            </div>
+            <div class="exploreMain__exploreContainer__tokenTraffic__info">
+                <div class="exploreMain__exploreContainer__tokenTraffic__info__subject">“Blockchain”</div>
+                <div>le sujet tendance cette semaine</div>
+            </div>
+        </div>
+</main>
+
+
