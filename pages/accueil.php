d3be7afef021e897bb1eae8cbb93c0a0c5bae788
Romain Penchenat <romain.penchenat@gmail.com>
2019-12-13 19:20:05 +0100

    contenu landing

--- pages/accueil.php
+++ pages/accueil.php
@@ -1,6 +1,5 @@
 <div class="backgroundContainer"></div>
 <main class="landingMain">
-    <!-- <img class="landingBackgroundWave" src="assets/images/backgrounds_landing/fondegrade2.png" alt="background">  -->
     <div class="landingBackgroundContainer"></div>
     <div class="landingMain__landingContainer">
         <div class="landingMain__landingContainer__middleContainer">
@@ -9,13 +8,28 @@
             Partagez.
             </h2>
             <p class="landingMain__landingContainer__middleContainer__landingMainText">Devenez acteur de la nouvelle économie de la connaissance</p>
-            <a href="/explorer" class="primaryButton">Découvrir Savoir</a>
+            <a href="/explorer" class="primaryButton">Découvrir SAVOIR</a>
         </div>
     </div>
     <section class="landingMain__landingFirstSection">
         <img src="assets/images/backgrounds_landing/backgroundwhite.svg" alt="background" class="landingMain__landingFirstSection__firstSectionWave">
-        <h4 class="landingFirstSection__title">
+        <h4 class="landingFirstSection__subtitle">
             SAVOIR, faire grandir l'économie de la connaissance
         </h4>
+        <div class="landingMain__landingFirstSection__iconsList">
+            <div class="landingMain__landingFirstSection__iconsList__brainContainer iconContainer">
+                <img src="/assets/images/backgrounds_landing/brain-icon.svg" alt="">
+                <p>Un atout valorisable sur votre CV</p>
+            </div>
+            <div class="landingMain__landingFirstSection__iconsList__industryContainer iconContainer">
+                <img src="/assets/images/backgrounds_landing/industry-icon.svg" alt="">
+                <p>Un gage de qualité pour votre institution</p>
+            </div>
+            <div class="landingMain__landingFirstSection__iconsList__handsContainer iconContainer">
+                <img src="/assets/images/backgrounds_landing/hands-icon.svg" alt="">
+                <p>Un indicateur de développement 
+fiable</p>
+            </div>
+        </div>
     </section>
 </main>
