2df1c1dd7994813063dbe82c640fef1d4f760756
louplemaire <44206840+louplemaire@users.noreply.github.com>
2019-12-13 18:09:04 +0100

    :heavy_plus_sign: Src > Scripts > Explorer > Add subResearcherOpen

--- src/scripts/1_explorer.js
+++ src/scripts/1_explorer.js
@@ -1,1 +1,13 @@
 // Explorer
+
+// Open subresearcher
+const researcher = document.querySelector('.exploreMain__exploreContainer__explorer__researcher')
+const subResearcher = document.querySelector('.exploreMain__exploreContainer__explorer__subResearcher')
+const researcherList = document.querySelector('.exploreMain__exploreContainer__explorer__subResearcher__list')
+const subResearcherOpen = document.querySelector('.subResearcherOpen')
+const invisible = document.querySelector('.invisible')
+
+researcher.addEventListener('click', () => {
+    subResearcher.classList.toggle('subResearcherOpen')
+    researcherList.classList.toggle('invisible')
+})
