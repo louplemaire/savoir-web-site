aba25cf9d99f27c645b5cbd8789f1c956f10971c
Romain Penchenat <romain.penchenat@gmail.com>
2019-12-14 18:38:53 +0100

    Remove useless code

--- test/test.php
+++ test/test.php
@@ -1,101 +1,111 @@
 <?php
-// require '../vendor/autoload.php';
+/**
+ * Settings
+ */
+$node_url = 'http://213.202.230.42:8888/v1/';
 
-$transactionJsonModel = '{
-    "expiration": "2018-12-06T03:47:15",
-    "ref_block_num": 64484,
-    "ref_block_prefix": 2668208063,
-    "max_net_usage_words": 0,
-    "max_cpu_usage_ms": 0,
-    "delay_sec": 0,
-    "context_free_actions": [],
-    "actions": [
-      {
-        "account": "eosio.token",
-        "name": "transfer",
-        "authorization": [
-          {
-            "actor": "supereospark",
-            "permission": "active"
-          }
-        ],
-        "data": "00afa998aaabaac6a0986aff4b9a3c61102700000000000004454f5300000000057465737431"
-      }
-    ],
-    "transaction_extensions": [],
-    "signatures": [
-      "SIG_K1_JyxaXuABoAuzzaScAfw9evnQozYL17aBSUud4PEz4sRrRsPn2phwWuo9A1B14pRECoGcFKzF1dJHHvnM4YbUyKMaAt2agb"
-    ],
-    "context_free_data": []
-  }';
-
-$data = '{
-    "signatures": [
-      "SIG_K1_Kg8eUVeGbi5w3hR6vhmxhSbh2HLi7MWKpiEEeoQvbbab9W64Cq3VN6BvNdz6J8fEdeNkQRLnxD6v6Agy6vcXzqAfovJ4pR"
-    ],
-    "compression": "none",
-    "packed_context_free_data": "",
-    "packed_trx": "'.bin2hex($transactionJsonModel).'"
-  }';
-
-$data = [
-    'signatures' => [''],
-    'compression' => false,
-    'packed_context_free_data' => '',
-    'packed_trx' => bin2hex($transactionJsonModel),
-];
+$abiJsonToBin_url = $node_url.'chain/abi_json_to_bin';
+$pushTransaction_url = $node_url.'chain/push_transaction';
 
 echo "<pre>";
 
-var_dump($data);
+/**
+ * 
+ */
+$transactionJsonModel = '{
+  "expiration": "2018-12-06T03:47:15",
+  "ref_block_num": 64484,
+  "ref_block_prefix": 2668208063,
+  "max_net_usage_words": 0,
+  "max_cpu_usage_ms": 0,
+  "delay_sec": 0,
+  "context_free_actions": [],
+  "actions": [
+    {
+      "account": "eosio.token",
+      "name": "transfer",
+      "authorization": [
+        {
+          "actor": "supereospark",
+          "permission": "active"
+        }
+      ],
+      "data": "00afa998aaabaac6a0986aff4b9a3c61102700000000000004454f5300000000057465737431"
+    }
+  ],
+  "transaction_extensions": [],
+  "signatures": [
+    "SIG_K1_JyxaXuABoAuzzaScAfw9evnQozYL17aBSUud4PEz4sRrRsPn2phwWuo9A1B14pRECoGcFKzF1dJHHvnM4YbUyKMaAt2agb"
+  ],
+  "context_free_data": []
+}';
 
-// echo "<pre>---";
-// use \BlockMatrix\EosRpc\ChainFactory;
-// use \BlockMatrix\EosRpc\WalletFactory;
-// use \BlockMatrix\EosRpc\EosRpc;
+// $data = '{
+//   "signatures": [
+//     "SIG_K1_Kg8eUVeGbi5w3hR6vhmxhSbh2HLi7MWKpiEEeoQvbbab9W64Cq3VN6BvNdz6J8fEdeNkQRLnxD6v6Agy6vcXzqAfovJ4pR"
+//   ],
+//   "compression": "none",
+//   "packed_context_free_data": "",
+//   "packed_trx": "'.bin2hex($transactionJsonModel).'"
+// }';
 
-// $api = (new ChainFactory)->api();
-// // $eos->setWalletInfo("savoiradmin", "PW5Khtqw5uicxcVxjs13hhw3qiV9J6XuN6TXcFHcu5P5FCw7Yf4tP");
-// $walapi = (new WalletFactory)->api();
-// echo $walapi->open("savoiradmin");
-// echo "---------";
-// echo $walapi->unlock(["savoiradmin", "PW5Khtqw5uicxcVxjs13hhw3qiV9J6XuN6TXcFHcu5P5FCw7Yf4tP"]);
-// $eos = (new EosRpc($api, $walapi));
+$data = [
+  'signatures' => [
+    'SIG_K1_Kg8eUVeGbi5w3hR6vhmxhSbh2HLi7MWKpiEEeoQvbbab9W64Cq3VN6BvNdz6J8fEdeNkQRLnxD6v6Agy6vcXzqAfovJ4pR'
+  ],
+  'compression' => false,
+  'packed_context_free_data' => '',
+  'packed_trx' => bin2hex($transactionJsonModel),
+];
 
-// echo $api->getInfo();
-// var_dump($api->getAccount("hugoleroy123"));
-// echo $eos->transfer("projetsavoir", "hugoleroy123", "0.0001 SOR", "tinaelhawa__fra__93100_lebouclettespourleplaisirdetous");
+/**
+ * Get packed transaction
+ */
+$jsonRequest = '{"code":"projetsavoir", "action":"transfer", "args":{"from":"projetsavoir", "to":"hugoleroy123", "quantity":"0.0001 SOR", "memo":"hetic__fra__94160__Nom"}}';
+$abiJsonToBinOptions = array(
+    'http' => array(
+        'header'  => "Content-type: application/json\r\nAccept: application/json\r\n",
+        'method'  => 'POST',
+        'content' => $jsonRequest
+    )
+);
+$abiJsonToBinContext  = stream_context_create($abiJsonToBinOptions);
+$abiJsonToBinResult = json_decode(file_get_contents($abiJsonToBin_url, false, $abiJsonToBinContext),true);
+if ($abiJsonToBinResult === FALSE) { echo "error"; }
+$packedTransaction = $abiJsonToBinResult['binargs'];
+var_dump($packedTransaction);
 
-// echo $eos->pushAction("projetsavoir", "hi", ["projetsavoir"], ["actor"=>"projetsavoir","permission"=>"active"]);
+/**
+ * Set the transaction
+ */
+$transactionData = [
+    'signatures' => [
+      'SIG_K1_Kg8eUVeGbi5w3hR6vhmxhSbh2HLi7MWKpiEEeoQvbbab9W64Cq3VN6BvNdz6J8fEdeNkQRLnxD6v6Agy6vcXzqAfovJ4pR'
+    ],
+    "compression" => false,
+    "packed_context_free_data" => "",
+    "packed_trx" => $packedTransaction,
+];
 
-// $trx_ids = $eos->pushTransactions(
-//     [
-//         $eos->makeTransaction(
-//             [
-//                 [
-//                     "account" => "projetsavoir",
-//                     "name" => "transfer",
-//                     "authorization" => [
-//                         [
-//                             "actor" => "projetsavoir",
-//                             "permission" => "active"
-//                         ]
-//                     ],
-//                     "data" => [
-//                         "from" => "projetsavoir",
-//                         "to" => "hugoleroy123",
-//                         "quantity" => "0.0001 EOS",
-//                         "memo" => "tinaelhawa__fra__93100_lebouclettespourleplaisirdetous"
-//                     ]
-//                 ]
-//             ]
-//         )
-//     ]
-// );
-// var_dump($trx_ids);
-// foreach ($trx_ids as $key => $value) {
-//     echo $trx_ids[$key]['transaction_id'] . PHP_EOL;
-// }
+var_dump(json_encode($transactionData));
+
+/**
+ * Push the transaction
+ */
+$ch = curl_init();
+//set the url, number of POST vars, POST data
+curl_setopt($ch,CURLOPT_URL, $pushTransaction_url);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+  'Content-type' => 'application/json',
+  'Accept' => 'application/json'
+]);
+curl_setopt($ch,CURLOPT_POST, true);
+curl_setopt($ch,CURLOPT_POSTFIELDS, json_encode($transactionData));
+//So that curl_exec returns the contents of the cURL; rather than echoing it
+curl_setopt($ch,CURLOPT_RETURNTRANSFER, true); 
+//execute post
+$transactionResult = json_decode(curl_exec($ch),true);
+var_dump($transactionResult);
 
 echo "---</pre>";
 echo "hey";
--- vendor/manamine/php-eos-rpc-sdk/src/Exception/EosRpcThrowable.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace BlockMatrix\EosRpc\Exception;
-
-interface EosRpcThrowable extends \Throwable
-{
-}
