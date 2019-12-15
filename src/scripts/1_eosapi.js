27b7244f18a155f750e50f040df2dd8a7383ac2b
Romain Penchenat <romain.penchenat@gmail.com>
2019-12-15 16:37:03 +0100

    get user balance in js

--- src/scripts/1_eosapi.js
+++ src/scripts/1_eosapi.js
@@ -1,5 +1,20 @@
 class EosApi {
 
+    constructor() {
+        this.apiUrl = 'http://213.202.230.42:8888'
+    }
+
+    post(request,options,handler) {
+        fetch(`${this.apiUrl}/v1/${request}`, {
+            method: 'post',
+            body: JSON.stringify(options)
+        }).then(function(response) {
+            return response.json()
+        }).then(function(data) {
+            handler(data)
+        })
+    }
+
     getSorCurrentSupply() {
         // return the amount of tokens which were supplied
     }
@@ -8,12 +23,26 @@ class EosApi {
         // Return an array of the 10/20 last SAVOIR transactions
     }
 
-    getUserSorBalance(userName) {
-        // return the user amount of tokens
+    // Return the user amount of SOR tokens
+    getUserSorBalance(userName,handler) {
+        const options = {
+            'code':'projetsavoir',
+            'account':userName,
+            'symbol':'SOR'
+        }
+        this.post('chain/get_currency_balance',options,(data) => {
+            handler(data[0])
+        })
     }
 
-    getUserFullProfile() {
+    getUserFullProfil() {
         // return a user object with each category and all transactions
     }
 
 }
+
+let api = new EosApi
+
+api.getUserSorBalance('hugoleroy123', (balance) => {
+    console.log(balance)
+})
