e7ae4f8900d08d2c78249e111abda7af91a54379
louplemaire <44206840+louplemaire@users.noreply.github.com>
2019-12-16 16:52:28 +0100

    :arrow_up: Src > Scripts > Explorer > Upgrade querySelector

--- src/scripts/10_explorer.js
+++ src/scripts/10_explorer.js
@@ -5,7 +5,7 @@ const researcher = document.querySelector('.js-researcher')
 const subResearcher = document.querySelector('.js-subResearcher')
 const researcherList = document.querySelector('.js-subResearcher-list')
 const subResearcherOpen = document.querySelector('.subResearcherOpen')
-const invisible = document.querySelector('.invisible')
+
 if (researcher) {
     researcher.addEventListener('click', () => {
         subResearcher.classList.toggle('subResearcherOpen')
