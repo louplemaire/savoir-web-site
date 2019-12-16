fc359e62c59f13798f2ddccfe5c93299ae4d59a2
louplemaire <44206840+louplemaire@users.noreply.github.com>
2019-12-16 11:26:49 +0100

    :heavy_plus_sign: Pages > Profil > Add the front

---
+++ pages/profil.php
@@ -0,0 +1,48 @@
+<main class="profilMain">
+    <div class="profilMain__info">
+        <h2 class="profilMain__info__name">Nicolas Decreton</h2>
+        <h3 class="profilMain__info__sorNumber">130SOR</h3>
+    </div>
+    <div class="profilMain__savoirs">
+        <div class="profilMain__savoirs__backgroundProfil"></div>
+        <div class="profilMain__savoirs__title">
+            <div class="profilMain__savoirs__title__fakeDiv"></div>
+            <h3 class="profilMain__savoirs__title__name">Les savoirs de Nicolas</h3>
+            <div class="profilMain__savoirs__title__organize">
+                <span>Organiser par</span>
+                <span class="profilMain__savoirs__title__organize__filter">Quantité</span>
+                <img src="../assets/images/arrow.svg" alt="#">
+            </div>
+        </div>
+        <div class="profilMain__savoirs__specificity">
+            <ul class="profilMain__savoirs__specificity__list">
+                <li class="profilMain__savoirs__specificity__list__line">
+                    <div class="profilMain__savoirs__specificity__list__line__info">
+                        <div class="profilMain__savoirs__specificity__list__line__info__expertise">Dev web</div>
+                        <div>21 savoirs appris</div>
+                        <div>2 savoirs transmis</div>
+                    </div>
+                    <div class="profilMain__savoirs__specificity__list__line__number">80 S0R</div>
+                </li>
+
+                <li class="profilMain__savoirs__specificity__list__line">
+                    <div class="profilMain__savoirs__specificity__list__line__info">
+                        <div class="profilMain__savoirs__specificity__list__line__info__expertise">Dev web</div>
+                        <div>21 savoirs appris</div>
+                        <div>2 savoirs transmis</div>
+                    </div>
+                    <div class="profilMain__savoirs__specificity__list__line__number">80 S0R</div>
+                </li>
+
+                <li class="profilMain__savoirs__specificity__list__line">
+                    <div class="profilMain__savoirs__specificity__list__line__info">
+                        <div class="profilMain__savoirs__specificity__list__line__info__expertise">Dev web</div>
+                        <div>21 savoirs appris</div>
+                        <div>2 savoirs transmis</div>
+                    </div>
+                    <div class="profilMain__savoirs__specificity__list__line__number">80 S0R</div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</main>
