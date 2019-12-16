e2403ca5550bf5924b4f079fcbe181660144b6a9
louplemaire <44206840+louplemaire@users.noreply.github.com>
2019-12-16 15:14:26 +0100

    :heavy_plus_sign: Pages > Profil > Add profil detail

--- pages/profil.php
+++ pages/profil.php
@@ -1,4 +1,9 @@
+<div class="greyFilter"></div>
+<!-- H1 -->
+<h1>Le profil d'un utilisateur de SAVOIR</h1>
+<!-- Profil -->
 <main class="profilMain">
+    <!-- Name -->
     <div class="profilMain__info">
         <h2 class="profilMain__info__name">Nicolas Decreton</h2>
         <h3 class="profilMain__info__sorNumber">130SOR</h3>
@@ -14,6 +19,7 @@
                 <img src="../assets/images/arrow.svg" alt="#">
             </div>
         </div>
+        <!-- Specificity -->
         <div class="profilMain__savoirs__specificity">
             <ul class="profilMain__savoirs__specificity__list">
                 <li class="profilMain__savoirs__specificity__list__line">
@@ -43,6 +49,58 @@
                     <div class="profilMain__savoirs__specificity__list__line__number">80 S0R</div>
                 </li>
             </ul>
+        </div>
+    </div>
+    <!-- Profil detail -->
+    <div class="center">
+        <div class="profilMain__detail">
+            <div class="profilMain__detail__background"></div>
+            <div class="profilMain__detail__closeButton">Close</div>
+            <div class="profilMain__detail__info">
+                <div>
+                    <h2>Dev Web</h2>
+                    <div>Nicolas Decreton</div>
+                </div>
+                <div class="profilMain__detail__info__number">80 SOR</div>
+            </div>
+            <div class="profilMain__detail__token">
+                <div class="profilMain__detail__token__backgroundDetail"></div>
+                <div class="profilMain__detail__token__title">
+                    <h3>Savoirs “Dev Web”</h3>
+                    <div class="profilMain__detail__token__title__organize">
+                        <span>Organiser par</span>
+                        <span class="profilMain__detail__token__title__organize__filter">Quantité</span>
+                        <img src="../assets/images/arrow.svg" alt="#">
+                    </div>
+                </div>
+                <ul class="profilMain__detail__token__list">
+                    <li class="profilMain__detail__token__list__line">
+                        <div class="profilMain__detail__token__list__line__date">
+                            11/12/2019 à 10h26
+                        </div>
+                        <div class="profilMain__detail__token__list__line__deal">
+                            <div class="profilMain__detail__token__list__line__deal__mention">
+                                <div class="profilMain__detail__token__list__line__deal__mention__spec">“Conférence Histoire du numérique”</div>
+                                <div class="profilMain__detail__token__list__line__deal__mention__destination">Jack Jobs -> Téa Gates</div>
+                            </div>
+                            <div class="profilMain__detail__token__list__line__deal__value">2 SOR</div>
+                        </div>
+                    </li>
+
+                    <li class="profilMain__detail__token__list__line">
+                        <div class="profilMain__detail__token__list__line__date">
+                            11/12/2019 à 10h26
+                        </div>
+                        <div class="profilMain__detail__token__list__line__deal">
+                            <div class="profilMain__detail__token__list__line__deal__mention">
+                                <div class="profilMain__detail__token__list__line__deal__mention__spec">“Conférence Histoire du numérique”</div>
+                                <div class="profilMain__detail__token__list__line__deal__mention__destination">Jack Jobs -> Téa Gates</div>
+                            </div>
+                            <div class="profilMain__detail__token__list__line__deal__value">2 SOR</div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </main>
