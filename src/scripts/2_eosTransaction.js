51ce6ae7e27765e7ebc901907172d058f4c3df45
Romain Penchenat <romain.penchenat@gmail.com>
2019-12-16 09:43:14 +0100

    Conclude EosTransaction class

--- src/scripts/2_eosTransaction.js
+++ src/scripts/2_eosTransaction.js
@@ -2,20 +2,20 @@ class EosTransaction {
 
     constructor(data,receiverAccount) {
         if (data['action_trace'] && receiverAccount == data['action_trace']['act']['data']['to']) {
-            console.log("-----");
-            console.log(data);
+            // console.log("-----");
+            // console.log(data);
             let memo = data['action_trace']['act']['data']['memo'].split('__')
-            console.log(data['action_trace']['act']['data']['memo'])
-            console.log(memo)
+            // console.log(data['action_trace']['act']['data']['memo'])
+            // console.log(memo)
             this.destination = data['action_trace']['act']['data']['to']
+            this.date = `Le ${new Date(data['block_time']).toLocaleString('fr-fr')}`
             this.amount = EosApi.formatSorTokens(data['action_trace']['act']['data']['quantity'])
-            this.source = ''
-            this.category = ''
-            this.name = ''
+            this.source = memo[0]
+            this.category = memo[1]
+            this.country = memo[2]
+            this.zipcode = memo[3]
+            this.name = memo[4]
             this.isSorSender = (this.source == this.destination)
-            this.date = ''
-            this.country = ''
-            this.zipcode = ''
         } else {
             this.useless = true
         }
