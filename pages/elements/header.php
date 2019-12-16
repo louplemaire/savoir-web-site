19609bdd2afbfcb50d8fc393b6b2bbeeaad1bca0
Nicolas Decreton <47737539+NicolasDecreton@users.noreply.github.com>
2019-12-16 11:52:13 +0100

    account page

--- pages/elements/header.php
+++ pages/elements/header.php
@@ -2,7 +2,7 @@
     <a href="/accueil"><img src="../../assets/images/savoir-logo.png" alt="Savoir logo" class="header__logo"></a>
     <ul class="header__menu">
         <li><a href="/explorer">Explorer</a></li>
-        <li><a href="#">Créer un compte</a></li>
+        <li><a href="/account">Créer un compte</a></li>
         <li><a href="#">Intégrer SAVOIR</a></li>
         <li><a href="#">À propos</a></li>
     </ul>
