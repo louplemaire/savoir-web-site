d3d30e04e1601ac59ac99c84317fa04c472a5478
Romain Penchenat <romain.penchenat@gmail.com>
2019-12-17 16:58:18 +0100

    PHP envoi mail

---
+++ php/sendContactMail.action.php
@@ -0,0 +1,7 @@
+<?php
+$title = 'Prise de contact de '.$_POST['name'];
+$content = 'De : '.$_POST['name'].' : '.$_POST['email'].'
+
+'.$_POST['text'];
+$header = 'Reply-To: '.$_POST['email'];
+mail('romain.penchenat@gmail.com',$title,$content,$header);
