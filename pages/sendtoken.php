c20124e05d6388509d85a344554bde5e781527d2
HugoLeroyHL <hugo.leroy@hetic.net>
2019-12-17 18:04:47 +0100

    :tada: init sendtoken

---
+++ pages/sendtoken.php
@@ -0,0 +1,12 @@
+<main class="sendtokenMain">
+    <h2>Envoyer des Tokens</h2>
+    <div class="sendtokenMain__progressBar">
+        <div class="sendtokenMain__progressBar__1"></div>
+        <div class="sendtokenMain__progressBar__2"></div>
+        <div class="sendtokenMain__progressBar__3"></div>
+    </div>
+    <section class="sendtokenMain__sendtokenLeftSection">
+    </section>
+    <section class="sendtokenMain__sendtokenRightSection">
+    </section>
+</main> 
