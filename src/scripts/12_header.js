1b266c5e2bfe016197ffd8cecf55b2e1d90c4e3c
Nicolas Decreton <47737539+NicolasDecreton@users.noreply.github.com>
2019-12-17 16:54:10 +0100

    push menu

--- src/scripts/12_header.js
+++ src/scripts/12_header.js
@@ -1,8 +1,17 @@
 const menuIcons = document.querySelectorAll('.menuIcon'),
-    subMenu = document.querySelector('.subMenu')
+    subMenu = document.querySelector('.subMenu'),
+    menuSelect = document.querySelectorAll('.menuSelect')
 
 menuIcons.forEach(menuIcon => {
     menuIcon.addEventListener(
+        'click',
+        function(){
+            subMenu.classList.toggle('visible')
+        }
+    )      
+})
+menuSelect.forEach(menuSelect => {
+    menuSelect.addEventListener(
         'click',
         function(){
             subMenu.classList.toggle('visible')
