840876361f509dcc9e677e3dfcf2705150eaccb2
HugoLeroyHL <hugo.leroy@hetic.net>
2019-12-18 13:54:21 +0100

    :tada: validationsendtoken done

---
+++ pages/validationsendtoken.php
@@ -0,0 +1,7 @@
+<main class="validationMain">
+    <div class="validationMain__container js-mouseAnimated">
+        <img src="../assets/images/illustrations/validat.svg" alt="">
+        <h2>Vos tokens sont bien envoyé</h2>
+        <a href="/accueil" class="primaryButton primaryButtonValidation">Revenir à l'accueil</a>
+    </div>
+</main>
