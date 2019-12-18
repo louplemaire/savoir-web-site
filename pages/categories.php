072cbd8d120134ce6553fd11574c5bbfa6a3342d
Nicolas Decreton <47737539+NicolasDecreton@users.noreply.github.com>
2019-12-18 13:48:44 +0100

    moitié catégories

---
+++ pages/categories.php
@@ -0,0 +1,14 @@
+<main class="categoriesMain">
+    <div class="categoriesMain__categoriesContainer">
+        <div class="categoriesMain__categoriesContainer__explorer">
+            <input type="text" placeholder="Recherchez un utilisateur"class="categoriesMain__categoriesContainer__explorer__researcher js-researcher">
+            <div class="categoriesMain__categoriesContainer__explorerr__subResearcher js-researcher">
+                <ul class="categoriesMain__categoriesContainer__explorer__subResearcher__list js-subResearcher-list invisible">
+                    <li class="categoriesMain__categoriesContainer__explorer__subResearecher__list__line">
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </div>
+</main>
