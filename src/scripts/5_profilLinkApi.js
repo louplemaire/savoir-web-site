f661d905d32844a0181ce1685c796fd392856613
louplemaire <44206840+louplemaire@users.noreply.github.com>
2019-12-18 13:11:05 +0100

    :arrow_up: Srcc > Scripts > ProfilLinkApi > Upgrade the backend

--- src/scripts/5_profilLinkApi.js
+++ src/scripts/5_profilLinkApi.js
@@ -1,5 +1,124 @@
 const profilInfo = document.querySelector('.profilMain__info')
 
+function getCategoryDiv(category) {
+    // Créer la ligne
+    const line = document.createElement('li')
+    line.classList.add('profilMain__savoirs__specificity__list__line')
+
+    // Remplir la ligne
+    // Info
+    const info = document.createElement('div')
+    info.classList.add('profilMain__savoirs__specificity__list__line__info')
+    line.appendChild(info)
+
+    // // Expertise
+    const expertise = document.createElement('div')
+    expertise.classList.add('profilMain__savoirs__specificity__list__line__info__expertise')
+    expertise.innerText = category.savoirtopic
+    info.appendChild(expertise)
+
+    // // Token acquis
+    const tokenOwned = document.createElement('div')
+    tokenOwned.classList.add('profilMain__savoirs__specificity__list__line__info__expertise__tokenOwned')
+    tokenOwned.innerText = `${category.received} savoirs acquéris`
+    info.appendChild(tokenOwned)
+
+    // // Token transmis
+    const tokenGived = document.createElement('div')
+    tokenGived.classList.add('profilMain__savoirs__specificity__list__line__info__expertise__tokenGived')
+    tokenGived.innerText = `${category.send} savoirs transmis`
+    info.appendChild(tokenGived)
+
+    // // Valeur
+    const value = document.createElement('div')
+    value.classList.add('profilMain__savoirs__specificity__list__line__number')
+    value.innerText = `${category.tokensamount} SOR`
+    line.appendChild(value)
+
+    // // Ouvrir la catégorie
+    const closeProfilWindowButton = document.querySelector('.js-close-button')
+    const detailWindowGreyFilter = document.querySelector('.js-grey-filter')
+    const detailWindow = document.querySelector('.js-detail-window')
+    const overflowProfil = document.querySelector('.js-overflowProfil')
+
+    line.addEventListener('click', () => {
+        detailWindow.classList.remove('invisible')
+        detailWindowGreyFilter.classList.remove('invisible')
+        overflowProfil.classList.add('overflowHidden')
+        openDetailedCategory(category)
+    })
+
+    // Fermer la catégorie
+    if(closeProfilWindowButton){
+        closeProfilWindowButton.addEventListener('click', () => {
+            detailWindow.classList.add('invisible')
+            detailWindowGreyFilter.classList.add('invisible')
+            overflowProfil.classList.remove('overflowHidden')
+        })
+    }
+
+    return line
+}
+
+function openDetailedCategory(category) {
+    console.log(category)
+    // Remplir la catégorie
+    let api = new EosApi
+
+    api.getTransactionsOfUserForCategory(userAccount,category.savoirtopic,(transactions) => {
+        console.log(transactions)
+        const list = document.querySelector('.profilMain__detail__token__list')
+        transactions.forEach((transaction) => {
+            list.innerHTML = ""
+            list.appendChild(getTransactionCategoryDiv(transaction))
+        })
+    })
+}
+
+function getTransactionCategoryDiv(transaction) {
+    console.log(transaction)
+    // Créer la ligne
+    const line = document.createElement('li')
+    line.classList.add('profilMain__detail__token__list__line')
+
+    // Créer le contenu de la div de transaction
+    // Date
+    const date = document.createElement('div')
+    date.classList.add('profilMain__detail__token__list__line__date')
+    date.innerText = transaction.date
+    line.appendChild(date)
+
+    // Deal
+    const deal = document.createElement('div')
+    deal.classList.add('profilMain__detail__token__list__line__deal')
+    line.appendChild(deal)
+
+    // Mention
+    const mention = document.createElement('div')
+    mention.classList.add('profilMain__detail__token__list__line__deal__mention')
+    deal.appendChild(mention)
+
+    // Spec
+    const spec = document.createElement('div')
+    spec.classList.add('profilMain__detail__token__list__line__deal__mention__spec')
+    spec.innerText = transaction.name
+    mention.appendChild(spec)
+
+    // Destination
+    const destination = document.createElement('div')
+    destination.classList.add('profilMain__detail__token__list__line__deal__mention__destination')
+    destination.innerText = `${transaction.sender} -> ${transaction.receiver}`
+    mention.appendChild(destination)
+
+    // Value
+    const value = document.createElement('div')
+    value.classList.add('profilMain__detail__token__list__line__deal__value')
+    value.innerText = transaction.amount
+    deal.appendChild(value)
+
+    return line
+}
+
 if(profilInfo) {
     const api = new EosApi()
 
@@ -11,79 +130,13 @@ if(profilInfo) {
     titleUserName.innerText = `Les savoirs de ${userAccount}`
 
     api.getUserSorBalance(userAccount,(balance) => {
-        console.log(balance)
         userToken.innerText = `${balance} SOR`
     })
 
     api.getUserCategories(userAccount,(categories) => {
-        console.log(categories)
         const list = document.querySelector('.profilMain__savoirs__specificity__list')
         categories.forEach(category => {
-            // Récupérer la liste
             list.appendChild(getCategoryDiv(category))
         })
-
-        
-
-        function getCategoryDiv(category) {
-            // Créer la ligne
-            const line = document.createElement('li')
-            line.classList.add('profilMain__savoirs__specificity__list__line')
-            list.appendChild(line)
-
-            // Ouvrir la catégorie
-            const closeProfilWindowButton = document.querySelector('.js-close-button')
-            const detailWindowGreyFilter = document.querySelector('.js-grey-filter')
-            const detailWindow = document.querySelector('.js-detail-window')
-            const overflowProfil = document.querySelector('.js-overflowProfil')
-
-            line.addEventListener('click', () => {
-                detailWindow.classList.remove('invisible')
-                detailWindowGreyFilter.classList.remove('invisible')
-                overflowProfil.classList.add('overflowHidden')
-                console.log('ouvert');
-            })
-
-            // Fermer la catégorie
-            if(closeProfilWindowButton){
-                closeProfilWindowButton.addEventListener('click', () => {
-                    detailWindow.classList.add('invisible')
-                    detailWindowGreyFilter.classList.add('invisible')
-                    overflowProfil.classList.remove('overflowHidden')
-                })
-            }
-
-            // Remplir la ligne
-            // Info
-            const info = document.createElement('div')
-            info.classList.add('profilMain__savoirs__specificity__list__line__info')
-            line.appendChild(info)
-
-            // Expertise
-            const expertise = document.createElement('div')
-            expertise.classList.add('profilMain__savoirs__specificity__list__line__info__expertise')
-            expertise.innerText = category.savoirtopic
-            info.appendChild(expertise)
-
-            // Token acquis
-            const tokenOwned = document.createElement('div')
-            tokenOwned.classList.add('profilMain__savoirs__specificity__list__line__info__expertise__tokenOwned')
-            tokenOwned.innerText = `${category.received} savoirs acquéris`
-            info.appendChild(tokenOwned)
-
-            // Token transmis
-            const tokenGived = document.createElement('div')
-            tokenGived.classList.add('profilMain__savoirs__specificity__list__line__info__expertise__tokenGived')
-            tokenGived.innerText = `${category.send} savoirs transmis`
-            info.appendChild(tokenGived)
-
-            // Valeur
-            const value = document.createElement('div')
-            value.classList.add('profilMain__savoirs__specificity__list__line__number')
-            value.innerText = `${category.tokensamount} SOR`
-            line.appendChild(value)
-
-            return line
-        }
     })
 }
