966ae4a5a618a20b4b00691635911366cf980a71
HugoLeroyHL <hugo.leroy@hetic.net>
2019-12-18 11:35:00 +0100

    :tada: init validation

---
+++ pages/validationcontactus.php
@@ -0,0 +1,8 @@
+<main class="validationcontactusMain">
+    <div class="validationcontactusMain__container">
+        <img src="" alt="">
+        <h2>Nous avons bien reçu votre message !</h2>
+        <h2>A bientôt !</h2>
+        <a href="/accueil">Revenir à l'accueil</a>
+    </div>
+</main>
