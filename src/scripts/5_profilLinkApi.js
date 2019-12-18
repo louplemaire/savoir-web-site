3fc4c93f6d0a2282c11876de3d0138e4257c6131
louplemaire <44206840+louplemaire@users.noreply.github.com>
2019-12-18 01:18:52 +0100

    :arrow_up: Src > Srcipts > ProfilLinkApi > Upgrade the backend

--- src/scripts/5_profilLinkApi.js
+++ src/scripts/5_profilLinkApi.js
@@ -11,16 +11,50 @@ if(profilInfo) {
     titleUserName.innerText = `Les savoirs de ${userAccount}`
 
     api.getUserSorBalance(userAccount,(balance) => {
-        console.log(balance) // La quantité de token
+        console.log(balance)
         userToken.innerText = `${balance} SOR`
     })
 
     api.getUserCategories(userAccount,(categories) => {
         console.log(categories)
         categories.forEach(category => {
-            // Créer la cellule
-            // Remplir
-            // Ajouter en html
+            // Récupérer la liste
+            const list = document.querySelector('.profilMain__savoirs__specificity__list')
+
+            // Créer la ligne
+            const line = document.createElement('li')
+            line.classList.add('profilMain__savoirs__specificity__list__line')
+            list.appendChild(line)
+
+            // Remplir la ligne
+            // Info
+            const info = document.createElement('div')
+            info.classList.add('profilMain__savoirs__specificity__list__line__info')
+            line.appendChild(info)
+
+            // Expertise
+            const expertise = document.createElement('div')
+            expertise.classList.add('profilMain__savoirs__specificity__list__line__info__expertise')
+            expertise.innerText = category.savoirtopic
+            info.appendChild(expertise)
+
+            // Token acquis
+            const tokenOwned = document.createElement('div')
+            tokenOwned.classList.add('profilMain__savoirs__specificity__list__line__info__expertise__tokenOwned')
+            tokenOwned.innerText = `${category.received} savoirs acquéris`
+            info.appendChild(tokenOwned)
+
+            // Token transmis
+            const tokenGived = document.createElement('div')
+            tokenGived.classList.add('profilMain__savoirs__specificity__list__line__info__expertise__tokenGived')
+            tokenGived.innerText = `${category.send} savoirs transmis`
+            info.appendChild(tokenGived)
+
+            // Valeur
+            const value = document.createElement('div')
+            value.classList.add('profilMain__savoirs__specificity__list__line__number')
+            value.innerText = `${category.tokensamount} SOR`
+            line.appendChild(value)
         });
     })
 }
