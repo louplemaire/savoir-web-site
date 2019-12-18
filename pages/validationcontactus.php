e698e51a09e924bf4691507960b929a5c36fe781
HugoLeroyHL <hugo.leroy@hetic.net>
2019-12-18 12:11:24 +0100

    :rocket: validatoin page done

--- pages/validationcontactus.php
+++ pages/validationcontactus.php
@@ -1,8 +1,8 @@
 <main class="validationcontactusMain">
     <div class="validationcontactusMain__container">
-        <img src="" alt="">
+        <img src="../assets/images/illustrations/validat.svg" alt="">
         <h2>Nous avons bien reçu votre message !</h2>
         <h2>A bientôt !</h2>
-        <a href="/accueil">Revenir à l'accueil</a>
+        <a href="/accueil" class="primaryButton primaryButtonValidation">Revenir à l'accueil</a>
     </div>
 </main>
