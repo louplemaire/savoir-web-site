adf2a92b0ddabe6382e1aa27c1cf27910f864935
Romain Penchenat <romain.penchenat@gmail.com>
2019-12-19 21:55:29 +0100

    Improve meta head

--- index.php
+++ index.php
@@ -6,14 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="assets/images/favicon.png" />
 
-    <meta name="description" content="">
+    <meta name="description" content="SAVOIR vous accompagne dans la transition vers la nouvelle économie du savoir en vous permettant de garder un historique de vos acquis tout en mettant en avant le partage de ceux-ci.">
     <!-- Balises réseaux sociaux -->
     <meta property="og:image" content="">
-    <meta property="og:title" content="Nom du site">
-    <meta property="og:description" content="">
+    <meta property="og:title" content="SAVOIR - Vivez l'économie du savoir">
+    <meta property="og:description" content="SAVOIR vous accompagne dans la transition vers la nouvelle économie du savoir en vous permettant de garder un historique de vos acquis tout en mettant en avant le partage de ceux-ci.">
     <meta property="og:url" content="">
     <meta property="og:locale" content="fr_FR">
-    <meta property="og:site_name" content="Nom du site">
+    <meta property="og:site_name" content="SAVOIR - Vivez l'économie du savoir">
     <meta property="og:type" content="website">
     <link rel="stylesheet" href="/assets/styles/main.min.css">
 </head>
--- php/rooter.php
+++ php/rooter.php
@@ -1,7 +1,8 @@
 <?php
+// $url = (key_exists('REDIRECT_URL',$_SERVER)) ? $_SERVER['REDIRECT_URL'] : "";
 $url = (key_exists('PATH_INFO',$_SERVER)) ? $_SERVER['PATH_INFO'] : "";
 if ($url == "" || $url == "/") {
     include_once 'pages/accueil.php';
 } else {
-    include_once 'pages'.$_SERVER['PATH_INFO'].'.php';
+    include_once 'pages'.$url.'.php';
 }
