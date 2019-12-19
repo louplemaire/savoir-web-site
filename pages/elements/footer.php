d3eec7e23264d7955f974c9d71281d1d2514b012
Nicolas Decreton <47737539+NicolasDecreton@users.noreply.github.com>
2019-12-19 11:17:38 +0100

    conflit

---
+++ pages/elements/footer.php
@@ -0,0 +1,9 @@
+<footer class="footerMain">
+    <ul>
+        <li class="footerLinks"><a href="https://github.com/louplemaire/savoir-web-site" target="_blank">Notre github</a></li>
+        <li class="footerLinks"><a href="https://eos.io/" target="_blank">Blockchain EOS</a></li>
+        <li class="footerLinks"><a href="/integrate">Intégration pour entreprises</a></li>
+        <li class="footerLinks"><a href="">Mentions légales</a></li>
+        <li class="footerLinks"><a href="/about">À propos</a></li>
+    </ul>
+</footer>
--- pages/integrate.php
+++ pages/integrate.php
@@ -1,39 +1,39 @@
 <main class="integrateMain">
-    <section class="integrateMain__integrateLeftSection js-mouseAnimated">
-        <h2 class="integrateTitle">Intégrer SAVOIR</h2>
-        <div class="integrateMain__integrateLeftSection__container">
-            <div class="integrateMain__integrateLeftSection__container__verticalBar">
+    <div class="integrateMain__sectionContainer">
+        <section class="integrateMain__sectionContainer__integrateLeftSection js-mouseAnimated">
+            <h2 class="integrateTitle">Intégrer SAVOIR</h2>
+            <div class="integrateMain__sectionContainer__integrateLeftSection__container">
+                <div class="integrateMain__sectionContainer__integrateLeftSection__container__verticalBar">
+                </div>
+                <div class="integrateMain__sectionContainer__integrateLeftSection__container__rightContainer">
+                    <div class="integrateMain__sectionContainer__integrateLeftSection__container__rightContainer__paragraph">
+                        <div class="integrateMain__sectionContainer__integrateLeftSection__container__rightContainer__paragraph__bulletPoint"></div>
+                        <h3>Valorisez vos employés</h3>
+                        <p>Montrez les bénéfices de l’apprentissage par formation et le progrès au sein de l’entreprise</p>
+                    </div>
+                    <div class="integrateMain__sectionContainer__integrateLeftSection__container__rightContainer__paragraph">
+                    <div class="integrateMain__sectionContainer__integrateLeftSection__container__rightContainer__paragraph__bulletPoint"></div>
+                        <h3>Encouragez vos élèves</h3>
+                        <p>Partagez la qualité de l’enseignement de votre établissement et l’initiative de vos élèves</p>
+                    </div>
+                    <div class="integrateMain__sectionContainer__integrateLeftSection__container__rightContainer__paragraph">
+                    <div class="integrateMain__sectionContainer__integrateLeftSection__container__rightContainer__paragraph__bulletPoint"></div>
+                    <h3>Rankez votre organisation</h3>
+                    <p>Montrez l’influence de votre organisation ainsi que son rayonnement de connaissances à toutes les échelles</p>
+                    </div>
+                    <div class="integrateMain__sectionContainer__integrateLeftSection__container__rightContainer__paragraph">
+                        <div class="integrateMain__sectionContainer__integrateLeftSection__container__rightContainer__paragraph__bulletPoint"></div>
+                        <h3>Intégrez SAVOIR gratuitement avec notre aide</h3>
+                        <p>Grâce à notre API, intégrez gratuitement SAVOIR à votre site, et bénéficier d’une assistance gratuite afin de vous aider à la mise en place </p>
+                    </div>
+                </div>
             </div>
-            <div class="integrateMain__integrateLeftSection__container__rightContainer">
-                <div class="integrateMain__integrateLeftSection__container__rightContainer__paragraph">
-                    <div class="integrateMain__integrateLeftSection__container__rightContainer__paragraph__bulletPoint"></div>
-                    <h3>Valorisez vos employés</h3>
-                    <p>Montrez les bénéfices de l’apprentissage par formation et le progrès au sein de l’entreprise</p>
-                </div>
-                <div class="integrateMain__integrateLeftSection__container__rightContainer__paragraph">
-                <div class="integrateMain__integrateLeftSection__container__rightContainer__paragraph__bulletPoint"></div>
-                    <h3>Encouragez vos élèves</h3>
-                    <p>Partagez la qualité de l’enseignement de votre établissement et l’initiative de vos élèves</p>
-                </div>
-                <div class="integrateMain__integrateLeftSection__container__rightContainer__paragraph">
-                <div class="integrateMain__integrateLeftSection__container__rightContainer__paragraph__bulletPoint"></div>
-                <h3>Rankez votre organisation</h3>
-                <p>Montrez l’influence de votre organisation ainsi que son rayonnement de connaissances à toutes les échelles</p>
-                </div>
-                <div class="integrateMain__integrateLeftSection__container__rightContainer__paragraph">
-                    <div class="integrateMain__integrateLeftSection__container__rightContainer__paragraph__bulletPoint"></div>
-                    <h3>Intégrez SAVOIR gratuitement avec notre aide</h3>
-                    <p>Grâce à notre API, intégrez gratuitement SAVOIR à votre site, et bénéficier d’une assistance gratuite afin de vous aider à la mise en place </p>
-
-                </div>
-            </div>
+        </section>
+        <section class="integrateMain__sectionContainer__integrateRightSection">
+            <img src="assets/images/illustrations/knowledge_icon.svg" alt="woman thinking">
+        </section>
         </div>
-    </section>
-    <section class="integrateMain__integrateRightSection">
-        <img src="assets/images/illustrations/knowledge_icon.svg" alt="woman thinking">
-    </section>
     <section class="integrateMain__bottomSection">
-         <img src="assets/images/backgrounds_landing/bluewavebg.svg" alt="background">
          <div class="bulletPoint js-mouseAnimated">
              <div class="reverseBar"></div>
          </div>
