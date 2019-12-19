73abf0de93bbd17c299850ccd24301760d99ed02
Nicolas Decreton <47737539+NicolasDecreton@users.noreply.github.com>
2019-12-19 16:40:06 +0100

    resp footer

--- pages/elements/footer.php
+++ pages/elements/footer.php
@@ -1,8 +1,8 @@
 <footer class="footerMain">
     <ul>
         <li class="footerLinks"><a href="https://github.com/louplemaire/savoir-web-site" target="_blank">Notre github</a></li>
-        <li class="footerLinks"><a href="https://eos.io/" target="_blank">Blockchain EOS</a></li>
-        <li class="footerLinks"><a href="/integrer_savoir">Intégration pour entreprises</a></li>
+        <li class="footerLinks optionalLink"><a href="https://eos.io/" target="_blank">Blockchain EOS</a></li>
+        <li class="footerLinks optionalLink"><a href="/integrer_savoir">Intégration pour entreprises</a></li>
         <li class="footerLinks"><a href="">Mentions légales</a></li>
         <li class="footerLinks"><a href="/a_propos">À propos</a></li>
     </ul>
